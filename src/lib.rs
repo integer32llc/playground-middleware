9fbbfe3ecd4052fb52fe44dd670bc0c2276e4a75
Jake Goulding <jake.goulding@gmail.com>
2016-11-08 23:37:18 -0500

    Add statistic logging extracted from the playground

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
 #[macro_use]
 extern crate log;
+
+extern crate csv;
 extern crate iron;
-extern crate time;
 extern crate mime;
 extern crate mime_guess;
+extern crate rustc_serialize;
+extern crate time;
 
 mod cache;
 mod guess_content_type;
+mod logging;
 mod modify_with;
 mod prefix;
 mod staticfile;
 
 pub use cache::Cache;
 pub use guess_content_type::GuessContentType;
+pub use logging::{StatisticLogger, FileLogger};
 pub use modify_with::ModifyWith;
 pub use prefix::Prefix;
 pub use staticfile::Staticfile;
