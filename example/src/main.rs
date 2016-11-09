0628a414cbeb4cfef2befcff4db916fb70c1494b
Jake Goulding <jake.goulding@gmail.com>
2016-11-08 22:56:43 -0500

    Guess content types based on the request path

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,7 +3,7 @@ extern crate iron;
 extern crate staticfile_adv;
 
 use iron::prelude::*;
-use staticfile_adv::{Staticfile, Prefix, ModifyWith, Cache};
+use staticfile_adv::{Staticfile, Prefix, ModifyWith, Cache, GuessContentType};
 use std::time::Duration;
 
 const ADDRESS: &'static str = "127.0.0.1:8000";
@@ -16,8 +16,12 @@ fn main() {
 
     let one_day = Duration::new(60 * 60 * 24, 0);
     let one_year = Duration::new(60 * 60 * 24 * 365, 0);
+    let default_content_type = "text/html".parse()
+        .expect("Unable to create default content type");
+
     files.link_after(ModifyWith::new(Cache::new(one_day)));
     files.link_after(Prefix::new(&["assets"], Cache::new(one_year)));
+    files.link_after(GuessContentType::new(default_content_type));
 
     let _server = Iron::new(files).http(ADDRESS).expect("Unable to start server");
     println!("Server listening at {}", ADDRESS);
