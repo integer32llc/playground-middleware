f8c854fc2efde95c3636fdca9a8f1e37411b95be
Jake Goulding <jake.goulding@gmail.com>
2016-11-08 20:06:07 -0500

    Limit example caching to one day

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,7 +3,8 @@ extern crate iron;
 extern crate staticfile_adv;
 
 use iron::prelude::*;
-use staticfile_adv::{Staticfile, Prefix, Cache};
+use staticfile_adv::{Staticfile, Prefix, ModifyWith, Cache};
+use std::time::Duration;
 
 const ADDRESS: &'static str = "127.0.0.1:8000";
 
@@ -13,7 +14,9 @@ fn main() {
     let files = Staticfile::new("./files").expect("Directory to serve not found");
     let mut files = Chain::new(files);
 
-    let one_year = ::std::time::Duration::new(60 * 60* 24 * 365, 0);
+    let one_day = Duration::new(60 * 60 * 24, 0);
+    let one_year = Duration::new(60 * 60 * 24 * 365, 0);
+    files.link_after(ModifyWith::new(Cache::new(one_day)));
     files.link_after(Prefix::new(&["assets"], Cache::new(one_year)));
 
     let _server = Iron::new(files).http(ADDRESS).expect("Unable to start server");
