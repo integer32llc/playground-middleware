1c8b31d30cc7e1ccb10eee638bf861fc5149dada
Jake Goulding <jake.goulding@gmail.com>
2017-03-20 21:33:22 -0400

    Add ability to redirect SPA URLs to the same static file

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,14 @@ extern crate mime;
 extern crate mime_guess;
 extern crate rustc_serialize;
 extern crate time;
+extern crate url;
 
 mod cache;
 mod guess_content_type;
 mod logging;
 mod modify_with;
 mod prefix;
+mod rewrite;
 mod staticfile;
 
 pub use cache::Cache;
@@ -20,4 +22,5 @@ pub use guess_content_type::GuessContentType;
 pub use logging::{StatisticLogger, FileLogger};
 pub use modify_with::ModifyWith;
 pub use prefix::Prefix;
+pub use rewrite::Rewrite;
 pub use staticfile::Staticfile;
